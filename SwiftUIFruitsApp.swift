fc09e8a73dd00fc2438b2a66797eb23ffc14468d
Blake Kvarfordt <blakekvarfordt@gmail.com>
2021-03-11 18:05:09 -0800

    initial commit

---
+++ SwiftUIFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFruitsApp.swift
+//  SwiftUIFruits
+//
+//  Created by Blake Kvarfordt on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
