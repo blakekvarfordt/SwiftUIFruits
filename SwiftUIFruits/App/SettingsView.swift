fcff2cf4c0476dfd923a3464dc66d9d4529e2933
Blake Kvarfordt <blakekvarfordt@gmail.com>
2021-04-02 15:13:46 -0700

    changes

--- SwiftUIFruits/App/SettingsView.swift
+++ SwiftUIFruits/App/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     var body: some View {
         NavigationView {
@@ -34,6 +35,36 @@ struct SettingsView: View {
                             Text("Fruits have lots of nutrients. I hate them when I just want to eat food. Fruits are shiiiiiiiit")
                         }
                     }
+                    
+                    GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        Text("If you wish, you can restart the application by toggling the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                        
+                    }
+                    
                     
                     GroupBox(
                         label:
